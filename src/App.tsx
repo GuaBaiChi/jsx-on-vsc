db8b2fc539dd17e6de1d351127ea3da28f30888d
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2023-07-20 16:10:58 -0400

    moved all pokedex files to it's own repository
    moved all pokedex files to it's own repository

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import ApiReadOG from './Components/ApiReadOG';
 import ApiReadAxios from './Components/ApiReadAxios';
 import ApiReadFetch from './Components/ApiReadFetch';
 import Yousaf from './Components/Yousaf';
-import Poke from './Pokedex/Poke';
 
 
 function App() {
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <Name /> */}
-      {/* <ApiReadOG /> */}
-      {/* <ApiReadFetch /> */}
-      {/* <ApiReadAxios /> */}
-      {/* <Yousaf /> */}
-      < Poke />
+      <Counter />
+      <Name />
+      <ApiReadOG />
+      <ApiReadFetch />
+      <ApiReadAxios />
+      <Yousaf />
     </div>
   );
 }
